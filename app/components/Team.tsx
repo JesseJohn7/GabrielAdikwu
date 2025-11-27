99a4cc34e53e8fadb61372d28a278ffe71576ad0
JesseJohn7 <jessejohn260@gmail.com>
2025-11-27 13:47:51 +0100

    name

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -13,20 +13,20 @@ interface Member {
 
 const teamMembers: Member[] = [
   {
-    name: "Barr Gabriel Adikwu",
+    name: "Barr Gabriel Christopher Adikwu ESQ",
     role: "Principal Attorney",
     image: "/adikwupict.jpg",
     bio: "Gabriel Adikwu is a lawyer whose career has evolved like a finely woven mosaic,stitched together by rigorous intellect,relentless advocacy and a vision that law,when rightly applied is a profession and a form of civic advocacy.For close to two decades from his call to the Nigerian Bar in 2008 and notarary public in 10 December in 2024 ,Adikwu has journeyed through a series of litigation , commercial arbitration and constitutional disputes. From courtrooms across various states of the federation and the Federal Capital Territory (FCT) to the august halls of the Court of Appeal and the Supreme Court of Nigeria his story is not simply that of a lawyer winnning cases but the story of a man steadily proven that the law is a technical instrument of society and an eternal dialogue between power and justice ",
 
   },
   {
-    name: "Esq Mercy Richard",
+    name: "Mercy Richard ESQ",
     role: "Head of Chamber",
     image: "/barrmercy.jpg",
     bio: "Mercy Richard is a distinguished graduate with honours from the prestigious University of Jos (LLB, Honours). She obtained her BL from the Nigerian Law School and was called to the Nigerian Bar in 2022. She is a practicing lawyer with extensive expertise in estate and property law, family law, corporate and commercial law, with a particular focus on litigation. Beyond her legal practice, Mercy is a passionate human rights activist and child rights advocate, committed to promoting justice and protecting the vulnerable. She is also an accomplished public speaker, arbitrator, and mediator, bringing professionalism and insight to every engagement. Mercy is an active member of the Nigerian Bar Association (NBA) and FIDA, Yola Branch, contributing significantly to the advancement of legal practice and the promotion of ethical standards within the profession.",
   },
   {
-    name: "Esq M.M Fattim",
+    name: "M.M Fattim ESQ",
     role: "Legal Advocate",
     image: "/fattim.jpg",
     bio: "Barr Miri Mercy Fattim is a young enthusiatic and resourceful legal practicioner called to the Nigerian Bar in 2025. She holds a Bachelor of Laws (LL.B) degree from Near East University (Cyprus), a Master of Laws (LLM) in international Law , a member of the international Law  , a member of international Law Association (ILA) an associate of the institute of chartered mediators and concilators (AICMC) amd completed her professional training at the Nigerian Law School. Her professional interests span across Litigation,Property Law ,International Commercial Law,International disputes settlement ,Arbitration and Maritime Law,with sound theoritical and field proficency .she exbits immense ability to perform Emperical Research and give practical suggestions reflecting a deep curiosity and progressive outlook towards emerging areas of law.",
